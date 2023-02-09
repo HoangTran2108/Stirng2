27384191faafda16b648d65a20589d82878b4726
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-02-09 14:40:40 +0700

    [Bài tập] Validate tên của lớp học

--- bt1/src/Main.java
+++ bt1/src/Main.java
@@ -3,12 +3,16 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class Main {
-    private static ClassNameExample classNameExample = new ClassNameExample();
+    private static String nameExample = "^[CAP]([0-9]{4})*[GHIKLM]$";
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập vào tên lớp ");
         String name = scanner.nextLine();
-        boolean isvalid = classNameExample.validate(name);
-        System.out.println("tên lớp " + name + " is valid: " + isvalid);
+        if(name.matches(nameExample)){
+            System.out.print("Tên lớp hợp lệ");
+        }
+        else{
+            System.out.print("Tên lớp không hợp lệ");
+        }
     }
 }
